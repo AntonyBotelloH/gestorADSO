b079a68e065c2682207ff39e336b30892e0fb9ea
AntonyBotelloH <antonybotello@gmail.com>
2026-03-13 16:13:33 -0500

    fix: corregir links de usuarios

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -4,10 +4,11 @@ from django.urls import path
 from usuarios.views import *
 
 urlpatterns = [
-    path('', inicio_usuario, name='inicio_usuario'),
+    path('usuario/', inicio_usuario, name='inicio_usuario'),
+    path('usuario/crear/', crear_usuario, name='crear_usuario'),
+    path('usuario/editar/<int:pk>/', editar_usuario, name='editar_usuario'),
     
-    path('nuevo/', crear_usuario, name='crear_usuario'),
-    path('editar/<int:pk>/', editar_usuario, name='editar_usuario'),
+    path('ficha/', inicio_ficha, name='inicio_ficha'),
+    path('fichas/crear/', crear_ficha, name='crear_ficha'),
     
-    path('fichas/nuevo/', crear_ficha, name='crear_ficha'),
 ]
