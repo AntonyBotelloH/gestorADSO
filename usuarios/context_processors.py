af8229e7630838cc74abc982c50367f088e17dd5
AntonyBotelloH <antonybotello@gmail.com>
2026-03-13 16:12:45 -0500

    feat: agregar funcionalidad para poder determinar la ficha actual

--- usuarios/context_processors.py
+++ usuarios/context_processors.py
@@ -1,12 +1,23 @@
 from .models import Ficha
 
 def fichas_globales(request):
-    """
-    Este procesador inyecta la lista de todas las fichas en TODAS las plantillas del proyecto.
-    """
-    # Traemos todas las fichas ordenadas por su código
+    # 1. Traemos todas las fichas para el selector
     fichas = Ficha.objects.all().order_by('codigo_ficha')
     
-    # Retornamos un diccionario. La llave 'lista_fichas' será la variable 
-    # que podrás usar en tu HTML.
-    return {'lista_fichas': fichas}
+    ficha_activa = None
+    # Como no cambiamos el nombre de la variable en la sesión, 
+    # sigue llamándose 'ficha_activa_id', pero ahora guarda el CÓDIGO (ej. "2555555")
+    codigo_seleccionado = request.session.get('ficha_activa_id')
+    
+    if codigo_seleccionado:
+        try:
+            # ¡CORRECCIÓN AQUÍ! Ahora busca por codigo_ficha
+            ficha_activa = Ficha.objects.get(codigo_ficha=codigo_seleccionado)
+        except Ficha.DoesNotExist:
+            # Si no existe, limpiamos la basura de la sesión
+            request.session['ficha_activa_id'] = None
+            
+    return {
+        'lista_fichas': fichas,
+        'ficha_activa': ficha_activa
+    }
