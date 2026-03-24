b111d470c7b3ffb8d1f0cdd815a58e464ac4fa61
AntonyBotelloH <antonybotello@gmail.com>
2026-03-24 17:38:18 -0500

    fix: ajustar modulo llamados

--- llamados/forms.py
+++ llamados/forms.py
@@ -2,6 +2,32 @@ from django import forms
 from .models import LlamadoAtencion, EstrategiaPedagogica, PlanMejoramiento, FaltaReglamento
 from usuarios.models import Usuario
 
+class PlanMejoramientoCrearForm(forms.ModelForm):
+    class Meta:
+        model = PlanMejoramiento
+        fields = ['estrategias', 'fecha_inicio', 'fecha_fin', 'observaciones']
+        
+        widgets = {
+            'estrategias': forms.SelectMultiple(attrs={
+                'class': 'form-select border-secondary-subtle bg-body-tertiary text-body shadow-none', 
+                'style': 'height: 120px;',
+                'help_text': 'Mantén presionado Ctrl (Windows) o Cmd (Mac) para seleccionar varias.'
+            }),
+            'fecha_inicio': forms.DateInput(attrs={
+                'type': 'date', 
+                'class': 'form-control border-secondary-subtle bg-body-tertiary text-body shadow-none'
+            }, format='%Y-%m-%d'),
+            'fecha_fin': forms.DateInput(attrs={
+                'type': 'date', 
+                'class': 'form-control border-secondary-subtle bg-body-tertiary text-body shadow-none'
+            }, format='%Y-%m-%d'),
+            'observaciones': forms.Textarea(attrs={
+                'class': 'form-control border-secondary-subtle bg-body-tertiary text-body shadow-none', 
+                'rows': 3, 
+                'placeholder': 'Instrucciones o notas adicionales para el aprendiz...'
+            }),
+        }
+
 class PlanMejoramientoForm(forms.ModelForm):
     class Meta:
         model = PlanMejoramiento
@@ -16,11 +42,11 @@ class PlanMejoramientoForm(forms.ModelForm):
             'fecha_inicio': forms.DateInput(attrs={
                 'type': 'date', 
                 'class': 'form-control border-secondary-subtle bg-body-tertiary text-body shadow-none'
-            }),
+            }, format='%Y-%m-%d'),
             'fecha_fin': forms.DateInput(attrs={
                 'type': 'date', 
                 'class': 'form-control border-secondary-subtle bg-body-tertiary text-body shadow-none'
-            }),
+            }, format='%Y-%m-%d'),
             'estado': forms.Select(attrs={
                 'class': 'form-select border-secondary-subtle bg-body-tertiary text-body shadow-none'
             }),
